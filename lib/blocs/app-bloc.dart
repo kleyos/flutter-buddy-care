8436a27e1e33c02125442f400e220fccd63931ee
Olha Ierushenko <olga.erushenko@gmail.com>
2019-04-29 17:53:46 +0300

    Add fcm

--- lib/blocs/app-bloc.dart
+++ lib/blocs/app-bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:collection';
 import 'package:buddy_care/blocs/bloc-provider.dart';
+import 'package:buddy_care/blocs/notif-bloc.dart';
 import 'package:buddy_care/services/api/post-api.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:buddy_care/models/post.dart';
@@ -8,6 +9,11 @@ import 'package:buddy_care/models/post.dart';
 class ApplicationBloc implements BlocBase {
   static ApplicationBloc _singleton;
 
+  BuddyCareBLoC _buddyCareBloC;
+  PushNotifBloc _pushNotifBloc;
+
+  BuddyCareBLoC get buddyCareBLoC => _buddyCareBloC ??= BuddyCareBLoC();
+  PushNotifBloc get pushNotBLoC => _pushNotifBloc ??= PushNotifBloc();
 
   factory ApplicationBloc() {
     if (_singleton != null) {
@@ -17,18 +23,22 @@ class ApplicationBloc implements BlocBase {
     return _singleton;
   }
 
-  ApplicationBloc._internal();
+  ApplicationBloc._internal(
+
+  );
 
 
 
   @override
   void dispose() {
     print('dispose Application block');
+    _buddyCareBloC.dispose();
+    _pushNotifBloc.dispose();
   }
 }
 
 
-class BuddyCareBLoC {
+class BuddyCareBLoC implements BlocBase {
   Map<String, dynamic> res;
   Stream<UnmodifiableListView<Post>> get posts => _postSubject.stream;
 
@@ -38,6 +48,11 @@ class BuddyCareBLoC {
     PostApi().getPosts().then((_res) => {
       _postSubject.add(UnmodifiableListView(_res))
     });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
   }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,65 @@
 import 'package:buddy_care/blocs/app-bloc.dart';
+import 'package:buddy_care/blocs/bloc-provider.dart';
+import 'package:buddy_care/blocs/notif-bloc.dart';
 import 'package:buddy_care/models/post.dart';
+import 'package:buddy_care/services/prefs-service.dart';
 import 'package:flutter/material.dart';
 import 'dart:collection';
 
-void main() {
-  final bcBloc = BuddyCareBLoC();
-  runApp(MyApp(bloc: bcBloc));
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  final prefsInstance = await SharedPreferences.getInstance();
+  PrefsService.init(prefsInstance);
+  PushNotifBloc.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final BuddyCareBLoC bloc;
   MyApp({
     Key key,
-    this.bloc,
   }): super(key: key);
 
 @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider<ApplicationBloc>(
+      bloc: ApplicationBloc(),
+      child: MaterialApp(
       title: 'BuddyCare',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Buddy Care', bloc: bloc,),
-    );
-  }
+      home: MyHomePage(title: 'Buddy Care'),
+    )
+    );}
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.bloc}) : super(key: key);
 
-  final BuddyCareBLoC bloc;
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget _buildItem(Post post) => Container(
-    padding: EdgeInsets.symmetric(vertical: 10.0),
-    child: Text(post.text),
-  );
+  ApplicationBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    PushNotifBloc().outNewMessages.listen(_onReceivePushNotification);
+  }
+
+  void _onReceivePushNotification(Map<String, dynamic> message) {
+    // TODO: implement tap on Push notification
+    print('---------- _onReceivePushNotification ---------- \n $message');
+  }
+
+  Widget _buildItem(Post post) =>
+    Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Text(post.text),
+    );
+
 
   @override
   Widget build(BuildContext context) {
+    _bloc = BlocProvider.of<ApplicationBloc>(context);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -52,12 +67,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: StreamBuilder<UnmodifiableListView<Post>>(
-        stream: widget.bloc.posts,
+        stream: _bloc.buddyCareBLoC.posts,
         initialData: UnmodifiableListView<Post>([]),
-        builder: (context, snapshot) => ListView(
-          children: snapshot.data.map(_buildItem).toList(),
-        )
+        builder: (context, snapshot) =>
+          ListView(
+            children: snapshot.data.map(_buildItem).toList(),
+          )
       )
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title }) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
